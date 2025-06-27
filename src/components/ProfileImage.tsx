9adac10e2f1e4022b7302a062a10d755cd51ed7a
nohyewon <yw4913@naver.com>
2025-06-27 17:37:44 +0900

    :sparkles:[feat]: 마이페이지 구현

---
+++ src/components/ProfileImage.tsx
@@ -0,0 +1,81 @@
+import React, { useState, useRef } from 'react';
+import styled from 'styled-components';
+import Profile from '@assets/svg/profile.svg?react';
+import Plus from '@assets/icons/Plus';
+
+interface ProfileImageProps {
+  imageUrl?: string;
+  isEdit?: boolean;
+  // eslint-disable-next-line no-unused-vars
+  onImageUpload: (imageUrl: string) => void;
+}
+
+const ProfileImage = ({
+  imageUrl,
+  isEdit = true,
+  onImageUpload,
+}: ProfileImageProps) => {
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(
+    imageUrl,
+  );
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+  const handlePlusClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (!files) return;
+
+    const imageUrl = URL.createObjectURL(files[0]);
+    setSelectedImage(imageUrl);
+    onImageUpload(imageUrl);
+  };
+
+  return (
+    <ProfileWrapper>
+      {selectedImage ? (
+        <ProfileImg src={selectedImage} alt="Profile" />
+      ) : (
+        <Profile />
+      )}
+      {isEdit && (
+        <PlusWrapper>
+          <Plus size={32} onClick={handlePlusClick} />
+        </PlusWrapper>
+      )}
+      <input
+        type="file"
+        ref={fileInputRef}
+        onChange={handleFileChange}
+        style={{ display: 'none' }}
+      />
+    </ProfileWrapper>
+  );
+};
+
+export default ProfileImage;
+
+const ProfileWrapper = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ProfileImg = styled.img`
+  width: 120px;
+  height: 120px;
+  border-radius: 50%;
+  object-fit: cover;
+`;
+
+const PlusWrapper = styled.div`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  transform: translate(10%, 0%);
+`;
---
+++ src/components/ProfileInfo.tsx
@@ -0,0 +1,69 @@
+import styled from 'styled-components';
+import { COLORS } from '@constants/colors';
+import { BlurBox } from './common';
+import ProfileImage from './ProfileImage';
+
+interface ProfileProps {
+  user_image: string;
+  name_k: string;
+  name_e: string;
+  gender: string;
+  age: number;
+  job: string;
+}
+
+const ProfileInfo = ({
+  user_image,
+  name_k,
+  name_e,
+  gender,
+  age,
+  job,
+}: ProfileProps) => {
+  return (
+    <Container>
+      <ProfileImage
+        imageUrl={user_image}
+        isEdit={false}
+        onImageUpload={() => {}}
+      />
+      <BlurBox
+        bgColor={COLORS.dark_green}
+        borderRadius="4rem"
+        style={{ marginTop: '-1.4rem', zIndex: -1 }}
+      >
+        <NameText>
+          {name_k || '김이름'} {name_e || 'Kim Name'}
+        </NameText>
+        <Text>성별: {gender || '여자'}</Text>
+        <Text>나이: {age || 20}</Text>
+        <Text>직업: {job || '학생'}</Text>
+      </BlurBox>
+    </Container>
+  );
+};
+
+export default ProfileInfo;
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+`;
+
+const NameText = styled.div`
+  font-size: 1.2rem;
+  color: ${COLORS.white};
+  text-align: center;
+  font-weight: bold;
+  margin-top: 1.6rem;
+  margin-bottom: 1rem;
+`;
+
+const Text = styled.div`
+  font-size: 0.8rem;
+  color: ${COLORS.white};
+  margin-bottom: 0.4rem;
+  margin-left: 3rem;
+`;
