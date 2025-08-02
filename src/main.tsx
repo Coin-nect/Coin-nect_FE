741e7d889482a224c1ca36042b370a429efb5727
nohyewon <yw4913@naver.com>
2025-08-02 23:03:29 +0900

    :art:[style]: font 설정

--- src/main.tsx
+++ src/main.tsx
@@ -13,6 +13,48 @@ const GlobalStyle = createGlobalStyle`
     justify-content: center;
     align-items: center;
   }
+
+  @font-face {
+    font-family: 'NanumHuman-Regular';
+    src: url('/fonts/NanumHumanRegular.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'NanumHuman-Bold';
+    src: url('/fonts/NanumHumanBold.woff') format('woff');
+    font-weight: bold;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'NanumHuman-Light';
+    src: url('/fonts/NanumHumanLight.woff') format('woff');
+    font-weight: 300;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'NanumHuman-ExtraLight';
+    src: url('/fonts/NanumHumanEL.woff') format('woff');
+    font-weight: 200;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'NanumHuman-ExtraBold';
+    src: url('/fonts/NanumHumanEB.woff') format('woff');
+    font-weight: 800;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'NanumHuman-Heavy';
+    src: url('/fonts/NanumHumanHeavy.woff') format('woff');
+    font-weight: 900;
+    font-style: normal;
+  }
 `;
 
 createRoot(document.getElementById('root')!).render(
